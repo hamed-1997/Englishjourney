dcabc5a1a09ae6274bf799c8b324de511ec29282
hamed-1997 <98464810+hamed-1997@users.noreply.github.com>
2026-08-26 15:53:12 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = "english-journey-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/app.js",
+  "./js/curriculum-data.js",
+  "./manifest.json",
+  "./assets/logo.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, resClone));
+        return res;
+      }).catch(() => cached);
+    })
+  );
+});
